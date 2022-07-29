7bdaafe15bed953c16a2ab17c24d27ae47e8fa7d
Edward Nunez <edwardnunez31@gmail.com>
2022-07-29 09:49:06 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+//declare var
+const express = require("express")
+const app = express()
+const PORT = 8500;
+const mongoose = require('mongoose')
+require('dotenv').config()
+//add model var 
+
+
+//add middlleware
+app.set("view engine", "ejs")
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+//set up the server 
+mongoose.connect(process.env.DB_CONNECTION,
+    {useNewUrlParser: true},
+    () => {console.log('connected to DB!')})
+
+
+
+
+app.listen(PORT, () => console.log(`server is runing on Port ${PORT}`))
+
+
+
+
+
